eab752cca55aea87abadc7b350b547b152430fc1
PaulJos5198 <143417270+PaulJos5198@users.noreply.github.com>
2024-05-09 19:23:19 +0530

    Initial commit

---
+++ sample.java
@@ -0,0 +1,10 @@
+public class HelloGuys {
+
+    public static void main (String[] args){
+      System.out.println("Hey Paul");
+      System.out.println("Hey Joseph");
+      System.out.println("Welcome");
+      System.out.println("Back");
+
+    }
+}
